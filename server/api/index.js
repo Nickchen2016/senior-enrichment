d3a84068d32546534b1a99243e58034d765cb324
nick Chen <chenyahua2012@hotmail.com>
2017-12-07 13:53:01 -0500

    first commit!

--- server/api/index.js
+++ server/api/index.js
@@ -1,12 +1,107 @@
 'use strict'
+const express = require('express');
 const apiRouter = require('express').Router()
-const db = require('../db')
+const db = require('../db/models')
+const Student = db.Student
+const Campus = db.Campus
+
 
 // If you aren't getting to this object, but rather the index.html (something with a joke) your path is wrong.
 	// I know this because we automatically send index.html for all requests that don't make sense in our backend.
 	// Ideally you would have something to handle this, so if you have time try that out!
-apiRouter.get('/hello', (req, res) => res.send({hello: 'world'}))
+//apiRouter.get('/hello', (req, res) => res.send({hello: 'world'}))
 
 // You can put all routes in this file; HOWEVER, this file should almost be like a table of contents for the routers you create
+
+//------------------Campus Routes-------------------------
+apiRouter.param('id', (req,res,next,id)=>{ //campus middleware
+	Campus.findById(id)
+	.then(campus=>{
+		if(campus){
+			req.campus = campus;
+			next();
+		}else{
+			const error = new Error('Student does not exist');
+			error.status = 404;
+			throw error;
+		}
+	})
+	.catch(next);
+});
+
+apiRouter.get('/campus', (req,res,next)=>{
+	Campus.findAll()
+	.then((result)=>{
+		res.send(result)
+	})
+	.catch(next)
+})
+
+apiRouter.get('/campus/:id', (req,res,next)=>{
+		res.send(req.campus)
+})
+
+apiRouter.post('/campus', (req,res,next)=>{
+	Campus.create(req.body)
+	.then((result)=>{
+		res.send(result)
+	})
+	.catch(next)
+})
+
+apiRouter.put('/campus/:id', (req,res,next)=>{
+	req.campus.update(req.body)
+	.then((result)=>{
+		res.send(result)
+	})
+	.catch(next)
+})
+
+apiRouter.delete('/campus/:id', (req,res,next)=>{
+	req.campus.destroy()
+	.then(() => res.sendStatus(204))
+	.catch(next)
+})
+
+ //------------------Student Routes-------------------------
+
+apiRouter.get('/student', (req,res,next)=>{
+	Student.findAll()
+	.then((result)=>{
+		res.send(result)
+	})
+	.catch(next)
+})
+
+apiRouter.get('/student/:id', (req,res,next)=>{
+	Student.findById(req.params.id)
+	.then(result=>{
+		res.send(result)
+	})
+	.catch(next)
+})
+
+apiRouter.post('/student', (req,res,next)=>{
+	Student.create(req.body)
+	.then((result)=>{
+		res.send(result)
+	})
+	.catch(next)
+})
+
+apiRouter.put('/student/:id', (req,res,next)=>{
+	Student.update(req.body,{where:{id:req.params.id}})
+	.then((result)=>{
+		res.send(result)
+	})
+	.catch(next)
+})
+
+apiRouter.delete('/student/:id', (req,res,next)=>{
+	Student.findById(req.params.id)
+	.then(result=>result.destroy())
+	.then(() => res.sendStatus(204))
+	.catch(next)
+})
 
 module.exports = apiRouter;
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,5 +1,5 @@
 'use strict';
-
+const Sequelize = require('sequelize');
 const db = require('../index');
 
 // Require all the models
@@ -7,6 +7,66 @@ const db = require('../index');
 	// This works if we all use the same Sequelize instance (instantiated in and exported from `/db/index.js`)
 	// Exporting all models from here seems like a good idea!
 
-// This is also probably a good place for you to set up your associations
+const Student = db.define('student', {
+	firstName:{
+		type: Sequelize.STRING,
+		allowNull: false,
+		validate:{
+			notEmpty: true
+		}
+	},
+	lastName:{
+		type: Sequelize.STRING,
+		allowNull: false,
+		validate:{
+			notEmpty: true
+		}
+	},
+	email:{
+		type: Sequelize.STRING,
+		allowNull: false,
+		validate:{
+			isEmail: true,
+			notEmpty: true
+		}
+	},
+	gpa: {
+		type: Sequelize.FLOAT,
+		validate:{
+			min:0.0,
+			max: 4.0
+		}
+	},
+	name:{
+		type: Sequelize.VIRTUAL,
+		get(){
+		return this.getDataValue('firstName')+' '+this.getDataValue('lastName')
+		}
+	}
+})
 
-module.exports = db
+const Campus = db.define('campus', {
+	name:{
+		type: Sequelize.STRING,
+		allowNull: false,
+		validate:{
+			notEmpty: true
+		}
+	},
+	imageUrl:{
+		type: Sequelize.STRING,
+		defaultValue: '',
+	},
+	description: {type:Sequelize.TEXT}
+})
+
+// This is also probably a good place for you to set up your associations
+Student.belongsTo(Campus);
+Campus.hasMany(Student);
+
+
+module.exports = {
+	db,
+	Student,
+	Campus
+}
